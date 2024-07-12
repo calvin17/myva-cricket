0be918a0e0e5f2636e21302fd641c3788b9d6f1d
calvin <calvin.abraham7@gmail.com>
2024-07-12 18:07:34 +0530

    fixed HMR

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,29 +1,69 @@
 const { merge } = require('webpack-merge');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
-  output: {
-    publicPath: 'http://localhost:8083/',
+  entry: './src/index',
+  devtool: 'source-map',
+  // output: {
+  //   publicPath: 'http://localhost:8083/',
+  // },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: true,
+          mangle: true,
+          output: {
+            comments: false,
+          },
+          sourceMap: true, 
+        },
+        extractComments: false,
+      }),
+    ],
   },
-  entry: './src/App.js',
   devServer: {
+    static: path.join(__dirname, 'dist'),
     port: 8083,
-    historyApiFallback: {
-      index: '/index.html',
+    open: false,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
     },
+    historyApiFallback: true,
+    // historyApiFallback: {
+    //   index: 'index.html',
+    // },
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'myva_cricket',
       filename: 'remoteEntry.js',
       exposes: {
-        './CricketIndex': './src/App.js',
+        './CricketIndex': './src/index.js',
       },
-      shared: packageJson.dependencies,
+      shared: {
+        ...packageJson.dependencies,
+        react: {
+          singleton: true,
+          requiredVersion: packageJson.dependencies.react,
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: packageJson.dependencies['react-dom'],
+        },
+      }
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import { Routes, Route, MemoryRouter } from 'react-router-dom';
+import React, { useState } from 'react';
+import { ThemeProvider } from '@emotion/react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+import lightTheme from './styles/themes/light';
 import CricketDashboard from './components/CricketDashboard';
 import MatchInfo from './components/MatchInfo';
+import SeriesList from './components/SeriesList';
+import LiveMatches from './components/LiveMatches';
+import SearchSeries from './components/SearchSeries';
+
+const queryClient = new QueryClient();
 
 export default () => {
+  const [theme, setTheme] = useState(lightTheme);
+
   return (
-    <div>
-      <MemoryRouter>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
         <Routes>
           <Route exact path="/match-info" element={<MatchInfo />} />
-          <Route path="/" element={<CricketDashboard />} />
+          <Route path="/" element={<CricketDashboard />}>
+            <Route path="/" element={<SeriesList />} />
+            <Route path="live" element={<LiveMatches />} />
+            <Route path="search" element={<SearchSeries />} />
+          </Route>
         </Routes>
-      </MemoryRouter>
-    </div>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 };
